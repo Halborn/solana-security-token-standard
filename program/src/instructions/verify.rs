76d5b57ec4bec60cb564160b838d31c975a0118b
Alexander Odegov <alexander.odegov@protonmail.com>
2025-10-01 19:53:58 +0400

    Remove the unused methods

--- program/src/instructions/verify.rs
+++ program/src/instructions/verify.rs
@@ -10,28 +10,16 @@ pub struct VerifyArgs {
 }
 
 impl VerifyArgs {
-    /// Parse VerifyArgs from raw instruction data - just one byte
+    /// Parse VerifyArgs from instruction data
     pub fn parse(data: &[u8]) -> Result<Self, ProgramError> {
         if data.is_empty() {
             return Err(ProgramError::InvalidInstructionData);
         }
-
         let discriminant = data[0];
         // Validate that discriminant is valid
         SecurityTokenInstruction::from_discriminant(discriminant)
             .ok_or(ProgramError::InvalidInstructionData)?;
 
         Ok(VerifyArgs { ix: discriminant })
-    }
-
-    /// Get the SecurityTokenInstruction from discriminant
-    pub fn get_instruction(&self) -> Result<SecurityTokenInstruction, ProgramError> {
-        SecurityTokenInstruction::from_discriminant(self.ix)
-            .ok_or(ProgramError::InvalidInstructionData)
-    }
-
-    /// Serialize to bytes - just one byte
-    pub fn to_bytes(&self) -> Vec<u8> {
-        vec![self.ix]
     }
 }
