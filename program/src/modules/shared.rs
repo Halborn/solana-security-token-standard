ea61c5d23a5b1c5ba67d9225450ef67a6886cbe5
Alexander Odegov <alexander.odegov@protonmail.com>
2025-10-16 14:37:36 +0400

    Resolve merge conflicts

--- program/src/modules/shared.rs
+++ program/src/modules/shared.rs
@@ -1,7 +1,7 @@
 use pinocchio::{account_info::AccountInfo, program_error::ProgramError, pubkey::Pubkey};
 use pinocchio_log::log;
 
-use crate::state::{AccountDeserialize, MintAuthority};
+use crate::state::MintAuthority;
 use crate::{acc_info_as_str, utils};
 
 /// Verify account as a signer, returning an error if it is not or if it is not writable while
--- program/src/state/mint_authority.rs
+++ program/src/state/mint_authority.rs
@@ -62,25 +62,6 @@ impl AccountDeserialize for MintAuthority {
 
         Ok(config)
     }
-
-    /// Parse from account info, verifying ownership and data length
-    pub fn from_account_info(
-        account_info: &AccountInfo,
-    ) -> Result<Ref<MintAuthority>, ProgramError> {
-        if account_info.data_len() < Self::LEN {
-            return Err(ProgramError::InvalidAccountData);
-        }
-
-        if !account_info.is_owned_by(&crate::ID) {
-            return Err(ProgramError::InvalidAccountOwner);
-        }
-
-        let data_ref = account_info.try_borrow_data()?;
-        let mint_authority = Self::try_from_bytes(&data_ref[..MintAuthority::LEN])?;
-        Ok(Ref::map(account_info.try_borrow_data()?, |_| {
-            &*Box::leak(Box::new(mint_authority))
-        }))
-    }
 }
 
 impl MintAuthority {
@@ -109,5 +90,24 @@ impl MintAuthority {
         }
 
         Ok(())
+    }
+
+    /// Parse from account info, verifying ownership and data length
+    pub fn from_account_info(
+        account_info: &AccountInfo,
+    ) -> Result<Ref<MintAuthority>, ProgramError> {
+        if account_info.data_len() < Self::LEN {
+            return Err(ProgramError::InvalidAccountData);
+        }
+
+        if !account_info.is_owned_by(&crate::ID) {
+            return Err(ProgramError::InvalidAccountOwner);
+        }
+
+        let data_ref = account_info.try_borrow_data()?;
+        let mint_authority = Self::try_from_bytes(&data_ref[..MintAuthority::LEN])?;
+        Ok(Ref::map(account_info.try_borrow_data()?, |_| {
+            &*Box::leak(Box::new(mint_authority))
+        }))
     }
 }
