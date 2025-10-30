8efb567af051cdbc9c8b6fa54b2775af1f84b065
Alexander Odegov <alexander.odegov@protonmail.com>
2025-10-30 17:39:04 +0400

    Do not cache target to avoid entrypoint conflicts

--- transfer_hook/src/lib.rs
+++ transfer_hook/src/lib.rs
@@ -33,7 +33,6 @@ const TRANSFER_VERIFICATION_CONFIG_DISCRIMINATOR: u8 = 1;
 // NOTE: Replace with the finalized program ID generated for the transfer hook deployment.
 declare_id!("DTUuEirVJFg53cKgyTPKtVgvi5SV5DCDQpvbmdwBtYdd");
 
-#[cfg(not(feature = "no-entrypoint"))]
 entrypoint!(process_instruction);
 
 fn process_instruction(
