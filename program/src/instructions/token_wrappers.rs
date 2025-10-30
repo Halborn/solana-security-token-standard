bbaed3525c6a109fa32713dcf0cd1b41397c90b3
Alexander Odegov <alexander.odegov@protonmail.com>
2025-10-30 16:26:36 +0400

    Simplify instruction data

--- program/src/instructions/token_wrappers.rs
+++ program/src/instructions/token_wrappers.rs
@@ -8,15 +8,6 @@ use pinocchio::instruction::{AccountMeta, Instruction, Signer};
 use pinocchio::ProgramResult;
 use pinocchio_token_2022::extensions::metadata::InitializeTokenMetadata;
 
-const UNINIT_BYTE: MaybeUninit<u8> = MaybeUninit::<u8>::uninit();
-
-#[inline(always)]
-fn write_bytes(destination: &mut [MaybeUninit<u8>], source: &[u8]) {
-    for (d, s) in destination.iter_mut().zip(source.iter()) {
-        d.write(*s);
-    }
-}
-
 /// Wrapper for RemoveKey instruction
 pub struct CustomRemoveKey<'a> {
     /// The metadata account to update.
@@ -327,19 +318,19 @@ impl<'a> CustomTransferChecked<'a> {
         // -  [0]: instruction discriminator (1 byte, u8)
         // -  [1..9]: amount (8 bytes, u64)
         // -  [9]: decimals (1 byte, u8)
-        let mut instruction_data = [UNINIT_BYTE; 10];
+        let mut instruction_data = [0u8; 10];
 
-        // Set discriminator as u8 at offset [0]
-        write_bytes(&mut instruction_data, &[12]);
-        // Set amount as u64 at offset [1..9]
-        write_bytes(&mut instruction_data[1..9], &self.amount.to_le_bytes());
-        // Set decimals as u8 at offset [9]
-        write_bytes(&mut instruction_data[9..], &[self.decimals]);
+        // Set discriminator at offset [0]
+        instruction_data[0] = 12;
+        // Set amount at offset [1..9]
+        instruction_data[1..9].copy_from_slice(&self.amount.to_le_bytes());
+        // Set decimals at offset [9]
+        instruction_data[9] = self.decimals;
 
         let instruction = Instruction {
             program_id: &pinocchio_token_2022::ID,
             accounts: &account_metas,
-            data: unsafe { from_raw_parts(instruction_data.as_ptr() as _, 10) },
+            data: &instruction_data,
         };
 
         invoke_signed(
