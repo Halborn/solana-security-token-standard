6a6cbe2b05c3eafb8f309373e5032197a6a7d1c1
Sergey Ponomarev <sergey@hoodies.team>
2025-11-27 12:49:18 +0300

    chore: remove one more unused fn

--- program/src/token22_extensions/mod.rs
+++ program/src/token22_extensions/mod.rs
@@ -11,16 +11,6 @@ use core::mem::MaybeUninit;
 
 const UNINIT_BYTE: MaybeUninit<u8> = MaybeUninit::<u8>::uninit();
 
-/// Deserialize a type from a byte array.
-///
-/// # Safety
-///
-/// This function is unsafe because it transmutes the input data to the output type.
-pub unsafe fn from_bytes<T: Clone + Copy>(data: &[u8]) -> T {
-    assert_eq!(data.len(), core::mem::size_of::<T>());
-    *(data.as_ptr() as *const T)
-}
-
 /// Deserialize a type from a byte array into a reference.
 ///
 /// # Safety
