f7168ccb7cbaab6b74b4ac4982552739b599657f
Alexander Odegov <alexander.odegov@protonmail.com>
2025-11-28 17:05:39 +0400

    Optimize execute_update_rate_account

--- program/src/modules/operations.rs
+++ program/src/modules/operations.rs
@@ -379,15 +379,11 @@ impl OperationsModule {
             return Err(ProgramError::NotEnoughAccountKeys);
         };
 
-        // For Split operation mint_from == mint_to
-        // If Rate was created for Convert operation, then mint_to should be verified
-        verify_operation_mint_info(verified_mint_info, &mint_to_info_account)?;
         verify_writable(rate_account_info)?;
         verify_owner(rate_account_info, program_id)?;
         verify_account_initialized(rate_account_info)?;
+        verify_operation_mint_info(verified_mint_info, &mint_to_info_account)?;
 
-        Mint::from_account_info(mint_from_account)?;
-        Mint::from_account_info(mint_to_info_account)?;
         let mint_from_key = mint_from_account.key();
         let mint_to_key = mint_to_info_account.key();
 
